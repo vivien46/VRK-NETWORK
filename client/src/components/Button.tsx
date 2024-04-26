8d79c44f889cb2b0f8d5148a5ab62e0273071097
vivien <vlad-vivien@orange.fr>
2024-04-26 15:42:52 +0200

    init tailwind css

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Définition de l'interface pour les props
+interface ButtonProps {
+  content: string;  // Texte du bouton
+}
+
+// Définition du composant Button avec des props typées
+const Button: React.FC<ButtonProps> = ({ content  }) => {
+  return (
+    <button className="bg-amber-400">{content}</button>  // Utilisation de 'color' comme style CSS du texte du bouton
+  );
+};
+
+export default Button;
+
